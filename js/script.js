5668cff55d2214c89c0ad3bda6cdfa94b5c17c10
ChristineWasike <christinewasike22@gmail.com>
2021-03-19 11:27:12 +0200

    Updated the website

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ function openNav() {
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
